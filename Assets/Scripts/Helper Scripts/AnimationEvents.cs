78282f1ebe3cb639ebc913eaa515e478fee419ca
pavanofficial1263 <pavanofficial1263@gmail.com>
2026-05-01 22:22:40 +0530

    Initial commit

---
+++ Assets/Scripts/Helper Scripts/AnimationEvents.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationEvents : MonoBehaviour {
+
+	private PlayerController playerController;
+	private Animator anim;
+
+	void Start () {
+		playerController = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void ResetShooting() {
+		playerController.canShoot = true;
+		anim.Play ("Idle");
+	}
+
+	void CameraStartGame() {
+		SceneManager.LoadScene ("Gameplay");
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Obstacle Scripts/ZombieScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour {
+
+	public GameObject bloodFXPrefab;
+	private float speed = 1f;
+
+	private Rigidbody myBody;
+
+	private bool isAlive;
+
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody> ();
+
+		speed = Random.Range (1f, 5f);
+
+		isAlive = true;
+	}
+
+	void Update () {
+
+		if (isAlive) {
+			myBody.linearVelocity = new Vector3 (0f, 0f, -speed);
+		}
+
+		if (transform.position.y < -10f) {
+			gameObject.SetActive (false);
+		}
+	}
+
+	void Die() {
+		isAlive = false;
+
+		myBody.linearVelocity = Vector3.zero;
+		GetComponent<Collider> ().enabled = false;
+		GetComponentInChildren<Animator> ().Play ("Idle");
+
+		transform.rotation = Quaternion.Euler (90f, 0f, 0f);
+		transform.localScale = new Vector3 (1f, 1f, 0.2f);
+		transform.position = new Vector3 (transform.position.x, 0.2f, transform.position.z);
+	}
+
+	void DeactivateGameObject() {
+		gameObject.SetActive (false);
+	}
+
+	void OnCollisionEnter(Collision target) {
+		if (target.gameObject.tag == "Player" || target.gameObject.tag == "Bullet") {
+			Instantiate (bloodFXPrefab, transform.position, Quaternion.identity);
+
+			Invoke ("DeactivateGameObject", 3f);
+
+			GameplayController.instance.IncreaseScore ();
+
+			Die ();
+
+		}
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
